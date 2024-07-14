67a22b66d7ab60c4e5700c242070ddb3efe16ee9
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-07-14 13:20:00 +0300

    5. Список кандидатов [#504842]

---
+++ src/main/java/dreamjob/controller/CandidateController.java
@@ -0,0 +1,21 @@
+package dreamjob.controller;
+
+import dreamjob.repository.CandidateRepository;
+import dreamjob.repository.MemoryCandidateRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/candidates")
+public class CandidateController {
+
+    private final CandidateRepository candidateRepository = MemoryCandidateRepository.getInstance();
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("candidates", candidateRepository.findAll());
+        return "candidates/list";
+    }
+}
---
+++ src/main/java/dreamjob/repository/CandidateRepository.java
@@ -0,0 +1,18 @@
+package dreamjob.repository;
+
+import dreamjob.model.Candidate;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface CandidateRepository {
+    Candidate save(Candidate candidate);
+
+    void deleteById(int id);
+
+    boolean update(Candidate candidate);
+
+    Optional<Candidate> findById(int id);
+
+    Collection<Candidate> findAll();
+}
