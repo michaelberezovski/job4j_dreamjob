23f657eb4bd28c49ac737d75b74b6cf5242f7e5c
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-07-22 11:55:57 +0300

    4. Создание вакансии [#504849 #506223]

--- src/main/java/dreamjob/controller/VacancyController.java
+++ src/main/java/dreamjob/controller/VacancyController.java
@@ -1,11 +1,17 @@
 package dreamjob.controller;
 
+import dreamjob.model.Vacancy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import dreamjob.repository.MemoryVacancyRepository;
 import dreamjob.repository.VacancyRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/vacancies")
@@ -22,5 +28,19 @@ public class VacancyController {
     @GetMapping("/create")
     public String getCreationPage() {
         return "vacancies/create";
+    }
+
+    @PostMapping("/create")
+    public String create(HttpServletRequest request) {
+        var title = request.getParameter("title");
+        var description = request.getParameter("description");
+        vacancyRepository.save(new Vacancy(0, title, description, LocalDateTime.now()));
+        return "redirect:/vacancies";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Vacancy vacancy) {
+        vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
     }
 }
